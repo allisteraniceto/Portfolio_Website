9b81fc7adde46218eed75db34980b17413472001
allisteraniceto <allisteraniceto@gmail.com>
2023-05-21 20:53:56 -0700

    added comments to javascript to get an understanding

--- src/app.js
+++ src/app.js
@@ -5,4 +5,4 @@ function toggleButton(){
     navList.classList.toggle('show') 
 }
 
-hamburgerButton.addEventListener('click', toggleButton)
+hamburgerButton.addEventListener('click', toggleButton) //"waits" for user to click, then calls toggleButton()
